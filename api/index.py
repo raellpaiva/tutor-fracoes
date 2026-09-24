915e2fd10c61f220fe559cb84d77263aa0743368
raellpaiva <contatoraellpaiva@gmail.com>
2026-09-24 20:41:19 -0300

    Ajusta rota da função Python no Vercel

--- api/index.py
+++ api/index.py
@@ -49,14 +49,14 @@ def identificar_erro(
     return "erro_nao_identificado"
 
 
-@app.get("/api")
+@app.get("/api/index")
 def tutoria():
     return {
         "mensagem": "API do Tutor de Frações funcionando!"
     }
 
 
-@app.post("/api")
+@app.post("/api/index")
 def receber_tentativa(tentativa: TentativaAluno):
 
     try:
